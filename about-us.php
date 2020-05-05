a547b35bde6828692a838db1bc125bda4155839a
Ubuntu <ubuntu@ip-172-31-52-92.ec2.internal>
2020-05-05 17:46:09 -0400

    page that explains the project and Team

---
+++ about-us.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="/cssFiles/about-usStyles.css">
+<body style="background-color:darkgray">
+
+<div class="wrapper">
+  <div id="container" class="clear">
+    <!-- ####################################################################################################### -->
+    <!-- ####################################################################################################### -->
+    <!-- ####################################################################################################### -->
+    <!-- ####################################################################################################### -->
+    <div id="about-us" class="clear">
+      <!-- ####################################################################################################### -->
+      <section id="about-intro" class="clear">
+        <h2>About Us</h2>
+        <img src="/cssFiles/rowanUniversityrobinson.jpg" alt="">
+        <p>The US National Vulnerability Database (NVD) added 12,174 Common Vulnerability and Exposure (CVE) entries in 2019.Each CVE entry acts as a universal identifier for publicly disclosed cybersecurity vulnerabilities and applicability to specific software and versions. The US Department of Homeland Security (DHS) maintains the National Cyber Awareness System, publishing important update bulletins and alerts for critical advanced persistent threat (APT) activity and other threat information.  This deluge of alerts can overwhelm any security engineer.  Regardless, the key to system cybersecurity, maintaining situational awareness, and continuous monitoring of a systems security posture requires constant observation of these security vulnerabilities, threats, and news feeds.</p>
+        <p>Our web application helps with this overwhelming issue with the deluge of alerts. Our website is called V.A.S.T, which stands for Vulnerability Alert System Tool.  V.A.S.T automates monitoring online sources of vulnerability information and alert feeds. The software filters the observed data to only relevant notifications based on applicable software, threat actors, or attack targets.   
+        A user can create an account, choose their filters and how frequently they want to be alerted. Any data deemed appropriate to a triggers the software to alert the security engineers, forwarding on the relevant data for their evaluation.</p>
+      </section>
+      <!-- ####################################################################################################### -->
+      <section id="team">
+        <h2>Our Team</h2>
+        <ul class="clear">
+          <li class="one_third first">
+            <!--<figure><img src="images/demo/team-member.gif" alt="">-->
+              <figcaption>
+                <p class="team_name"> Kiima Ballantyne</p>
+                <p class="team_title">Product Owner</p>
+                <p class="team_description">ballantyk1@students.rowan.edu</p>
+              </figcaption>
+            </figure>
+          </li>
+          <br>
+          <li class="one_third">
+            <!--<figure><img src="images/demo/team-member.gif" alt="">-->
+              <figcaption>
+                <p class="team_name">Anthony Catalina</p>
+                <p class="team_title">Scrum Master</p>
+                <p class="team_description">catalinaa2@students.rowan.edu</p>
+              </figcaption>
+            </figure>
+          </li>
+          <br>
+          <li class="one_third">
+            <!--<figure><img src="images/demo/team-member.gif" alt="">-->
+              <figcaption>
+                <p class="team_name">Isaiah Doyle</p>
+                <p class="team_title">Development team</p>
+                <p class="team_description">doylei19@students.rowan.edu</p>
+              </figcaption>
+            </figure>
+          </li>
+          <br>
+          <li class="one_third first">
+            <!--<figure><img src="images/demo/team-member.gif" alt="">-->
+              <figcaption>
+                <p class="team_name">Alexander Abbott</p>
+                <p class="team_title">Development team</p>
+                <p class="team_description">abbott88@students.rowan.edu</p>
+              </figcaption>
+            </figure>
+          </li>
+          <br>
+          <li class="one_third">
+            <!--<figure><img src="images/demo/team-member.gif" alt="">-->
+              <figcaption>
+                <p class="team_name">Jason Zogheb</p>
+                <p class="team_title">Development team</p>
+                <p class="team_description">zogheb89@students.rowan.edu</p>
+              </figcaption>
+            </figure>
+          </li>
+          <br>
+          <li class="one_third">
+            <!--<figure><img src="images/demo/team-member.gif" alt="">-->
+              <figcaption>
+                <p class="team_name">Benjamin Herrman</p>
+                <p class="team_title">Development team</p>
+                <p class="team_description">herrmanb6@students.rowan.edu</p>
+              </figcaption>
+            </figure>
+          </li>
+        </ul>
+      </section>
+	<footer style= "border-top: 1px solid black">
+	<a href="https://nvd.nist.gov/"
+		title= "Link to National Vulnerability Database">
+		National Vulnerability Database HomePage</a><br> 
+	<a href="https://www.us-cert.gov/ncas"
+		title= "Link to National Cyber Awareness System">
+		National Cyber Awareness System HomePage</a><br><br>
+	</footer>
+	
+      <!-- ####################################################################################################### -->
+    </div>
+    <!-- ####################################################################################################### -->
+    <!-- ####################################################################################################### -->
+    <!-- ####################################################################################################### -->
+    <!-- ####################################################################################################### -->
+  </div>
+</div>
+<div class="sidenav">
+  <a href="index.php">Home</a>
+ <!-- <a href="login.php">Login Page</a>-->
+  <a href="changePassword.php">Change Password</a>
+  <a href="editFilters.php">Edit Filters</a>
+  <a href="about-us.php">About</a>
+</div>
+
